aa6075c8706cccc6cb8deba12665ed1be2bfbfd0
Alexander Rojas <alexander@mesosphere.io>
2016-06-03 11:22:52 -0600

    Core Config for master working

--- master_config.py
+++ master_config.py
@@ -1,15 +1,33 @@
-class CoreConfig:
-    def __init__(self):
-        self.prefix = '/usr/bin/mesos-master'
+class ParseError(Exception):
+    def __init__(self, message):
+        self.message = message
 
     def __str__(self):
-        return self.prefix
+        return self.message
+
+class CoreConfig:
+    def __init__(self, yaml):
+        if 'work_dir' not in yaml:
+            raise ParseError('A working directory was not given.')
+
+        self.prefix = yaml['prefix'] if 'prefix' in yaml else '/usr/bin/mesos-master'
+        self.work_dir = yaml['work_dir']
+        self.web_ui = yaml['web_ui'] if 'web_ui' in yaml else None
+        self.cluster = yaml['cluster'] if 'cluster' in yaml else None
+
+    def __str__(self):
+        string = self.prefix
+        string += ' \\\n    --work_dir=' + self.work_dir
+        if self.web_ui:
+            string += ' \\\n    --web_ui=' + self.web_ui
+        if self.cluster:
+            string += ' \\\n    --cluster=' + self.cluster
+        return string
 
 
 class MasterConfig:
-    def __init__(self, yaml_object):
-        self.core_config = CoreConfig()
-        self.core_config.prefix = yaml_object['core']['prefix']
+    def __init__(self, yaml):
+        self.core = CoreConfig(yaml['core'])
 
     def __str__(self):
-        return str(self.core_config)
+        return '#!/usr/bin/env bash\n\n' + str(self.core)
